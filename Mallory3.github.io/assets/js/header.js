f356e56a4bccf376fed1a91457b7dcd25d39c82c
Richard <55150372+forurnv@users.noreply.github.com>
2019-12-08 12:59:03 -0700

    init 1

---
+++ Mallory3.github.io/assets/js/header.js
@@ -0,0 +1,14 @@
+const menuAppear = document.querySelector(".menu")
+const onClick = document.querySelector('.navMenu')
+menuAppear.addEventListener('click', function() {
+  console.log('appear')
+  onClick.className = "navMenu openState"
+})
+
+
+const menuDissappear = document.querySelector(".close")
+const closeClick = document.querySelector('.navMenu')
+menuDissappear.addEventListener('click', function() {
+  console.log('dissappear')
+  onClick.className = "navMenu closedState"
+})
